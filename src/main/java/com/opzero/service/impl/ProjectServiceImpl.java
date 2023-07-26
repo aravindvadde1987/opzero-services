3ef633ce626aaf1422600bd906867a3796088a14
Vadde <arvadde@deloitte.com>
2023-07-26 16:38:53 +0530

    added default values to project new fieilds

--- src/main/java/com/opzero/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/opzero/service/impl/ProjectServiceImpl.java
@@ -1,37 +1,47 @@
 package com.opzero.service.impl;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.opzero.entity.Project;
 import com.opzero.repository.ProjectRepository;
 import com.opzero.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProjectServiceImpl implements ProjectService {
-	@Autowired
-	ProjectRepository projectRepository;
+    @Autowired
+    ProjectRepository projectRepository;
 
-	@Override
-	public Project saveProject(Project project) {
-		return projectRepository.save(project);
-	}
+    @Override
+    public Project saveProject(Project project) {
+        setDefaultVal(project);
+        return projectRepository.save(project);
+    }
 
-	@Override
-	public Optional<Project> getProject(Long projectId) {
-		return projectRepository.findById(projectId);
-	}
+    private void setDefaultVal(Project project) {
+        project.setEngagementType("Fixed price");
+        project.setOffshoreSize(5L);
+        project.setOperatingModel("US-USI-DeliveryPool");
+        project.setScopeOfWork("Software Development");
+        project.setTeamSize(10L);
+        project.setOnshoreSize(5L);
+    }
 
-	@Override
-	public Project updateProject(Project project) {
-		return projectRepository.save(project);
-	}
+    @Override
+    public Optional<Project> getProject(Long projectId) {
+        return projectRepository.findById(projectId);
+    }
 
-	@Override
-	public List<Project> getProjects() {
-		return projectRepository.findAll();
-	}
+    @Override
+    public Project updateProject(Project project) {
+        setDefaultVal(project);
+        return projectRepository.save(project);
+    }
+
+    @Override
+    public List<Project> getProjects() {
+        return projectRepository.findAll();
+    }
 }
